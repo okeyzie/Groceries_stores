351b7566936145f92336fc3561e5bac01419a2d2
Okechukwu Gideon Kalu <59907714+okeyzie@users.noreply.github.com>
2025-08-12 22:03:15 +0100

    Update index.js
    postman changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get("/", (req, res) => {
     res.status(200).send({message: "Welcome to the my Grocery shop"});
 });
 
-//app.use(router);
+app.use(router);
 app.listen(PORT, () => {
     console.log(`Server running in port:${PORT}`);
 });
